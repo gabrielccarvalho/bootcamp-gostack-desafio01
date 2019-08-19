52358ba1e489acae1ed57ec008c49d2545406584
Gabriel Campos <gabrielcampos@Gabriels-MacBook-Pro.local>
2019-08-19 14:22:10 -0300

    chore: Added the post route

--- server.js
+++ server.js
@@ -10,4 +10,12 @@ app.get("/projects", (req, res) => {
   return res.json(projects);
 });
 
+app.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+
+  projects.push({ id, title });
+
+  return res.json(projects);
+});
+
 app.listen(3000);
