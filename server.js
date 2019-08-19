636bd631fc3be1ba7ce3654aff4aea7c5ceea8d8
Gabriel Campos <gabrielcampos@Gabriels-MacBook-Pro.local>
2019-08-19 16:58:23 -0300

    chore: Added a middleware to check if id exists

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ app.use(express.json());
 
 const projects = [];
 
+function checkIfIdExists(req, res, next) {
+  const { id } = req.params;
+  const project = projects.find(p => p.id == id);
+
+  if (!project) {
+    return res
+      .status(400)
+      .json({ error: "Id does not correspond to an existent project" });
+  }
+
+  return next();
+}
+
 app.get("/projects", (req, res) => {
   return res.json(projects);
 });
@@ -18,7 +31,7 @@ app.post("/projects", (req, res) => {
   return res.json(projects);
 });
 
-app.put("/projects/:id", (req, res) => {
+app.put("/projects/:id", checkIfIdExists, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
 
@@ -29,7 +42,7 @@ app.put("/projects/:id", (req, res) => {
   return res.json(project);
 });
 
-app.delete("/projects/:id", (req, res) => {
+app.delete("/projects/:id", checkIfIdExists, (req, res) => {
   const { id } = req.params;
 
   const index = projects.findIndex(p => p.id == `${id}`);
@@ -39,7 +52,7 @@ app.delete("/projects/:id", (req, res) => {
   return res.json(projects);
 });
 
-app.post("/projects/:id/tasks", (req, res) => {
+app.post("/projects/:id/tasks", checkIfIdExists, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
 
